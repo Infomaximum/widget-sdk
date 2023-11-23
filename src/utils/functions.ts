6d40436a35bc99b16e3d28876ad8f5dc8a88ce5c
Alexandr Petyaykin <saneksa2@gmail.com>
2023-11-23 11:16:56 +0300

    feat: добавлены утилитные функции

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -11,3 +11,18 @@ export const compactMap = <T, U>(
 
 export const isNil = (value: any): value is null | undefined =>
   value === null || value === undefined;
+
+export const first = <T>(arr: T[]): T | undefined => arr.at(0);
+
+export const identity = <T>(value: T): T => value;
+
+export function omit<T extends Record<keyof any, any>, K extends keyof T>(
+  obj: T,
+  keys: K[]
+): Omit<T, K> {
+  const newObj = { ...obj };
+
+  for (const key of keys) delete newObj[key];
+
+  return newObj as Omit<T, K>;
+}

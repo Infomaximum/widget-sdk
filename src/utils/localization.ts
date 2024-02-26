b862c54f521667261f201573703693df69e57a41
Alexandr Petyaykin <saneksa2@gmail.com>
2024-02-26 10:55:19 +0300

    refactor: jsdoc getLocalizedText

--- src/utils/localization.ts
+++ src/utils/localization.ts
@@ -11,6 +11,13 @@ export { type TLocalizationDescription } from "@infomaximum/localization";
 let localization: Localization | null = null;
 const supportedLanguages = [Localization.Language.ru, Localization.Language.en];
 
+/**
+ * Получает локализованный текст на основе переданного языка и объекта локализации.
+ * @param {ELanguages} language - Текущий язык, на котором должен быть возвращен локализованный текст.
+ * @param {L} locObj - Объект локализации, содержащий тексты на разных языках.
+ * @param {P} [props] - Дополнительные параметры локализации.
+ * @returns {string} - Локализованный текст для указанного языка.
+ */
 export const getLocalizedText = <
   L extends TLocalizationDescription,
   P extends ILocalizationProps = TExtractLocalizationParams<L>,

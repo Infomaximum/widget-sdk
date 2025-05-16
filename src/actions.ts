58e9f4994bf31c99eefe077935deb9482e68aa22
Dmitry Kuskov <dmtrkskv@gmail.com>
2025-05-16 13:39:39 +0300

    feat: добавлено поле dbDataType в настройки способов ввода
    Для обратной совместимости некоторое время будет поддерживаться вычисление dbDataType "на лету"

--- src/actions.ts
+++ src/actions.ts
@@ -57,6 +57,7 @@ export interface IParameterFromColumn {
   inputMethod: EWidgetActionInputMethod.COLUMN;
   tableName: string;
   columnName: string;
+  dbDataType?: string;
 }
 
 export interface IParameterFromVariable {
@@ -68,12 +69,14 @@ export interface IParameterFromFormula {
   inputMethod: EWidgetActionInputMethod.FORMULA;
   formula: string;
   considerFilters: boolean;
+  dbDataType?: string;
 }
 
 export interface IParameterFromAggregation {
   inputMethod: EWidgetActionInputMethod.AGGREGATION;
   formula: string;
   considerFilters: boolean;
+  dbDataType?: string;
 }
 
 export interface IParameterFromEvent {
@@ -92,6 +95,7 @@ export interface IParameterFromManualInput {
   inputMethod: EWidgetActionInputMethod.MANUALLY;
   description: string;
   defaultValue?: string;
+  dbDataType?: string;
   filterByRows?: boolean;
 }
 
@@ -105,6 +109,7 @@ export interface IParameterFromDynamicList {
   inputMethod: EWidgetActionInputMethod.DYNAMIC_LIST;
   options: string;
   defaultValue: string;
+  dbDataType?: string;
   displayOptions: string;
   filters: TExtendedFormulaFilterValue[];
   filterByRows?: boolean;

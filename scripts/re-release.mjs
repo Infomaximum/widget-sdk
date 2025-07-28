c48a0fdc5da96ad75c6eea9abc79a5c02ec7b357
Alexandr Petyaykin <a.petyaykin@infomaximum.com>
2025-07-28 10:36:38 +0300

    ci: хеш последнего коммита в версии

---
+++ scripts/re-release.mjs
@@ -0,0 +1,19 @@
+//@ts-check
+
+import { execSync } from "child_process";
+import { readFileSync } from "fs";
+import { resolve } from "path";
+import semver from "semver";
+
+const packageJsonPath = resolve(process.cwd(), "package.json");
+const packageData = JSON.parse(readFileSync(packageJsonPath, "utf-8"));
+const baseVersion = semver.valid(semver.coerce(packageData.version));
+
+const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
+const branchName = execSync("git rev-parse --abbrev-ref HEAD").toString().trim() || "unknown";
+
+const newVersion = `${baseVersion}-${commitHash}-${branchName}`;
+
+execSync(`npx standard-version --release-as ${newVersion} --skip.tag=false --skip.commit`, {
+  stdio: "inherit",
+});

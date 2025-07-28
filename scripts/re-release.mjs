1f8051d2e403c5ef6853cc3b29d8a9223c4aa8e0
Alexandr Petyaykin <a.petyaykin@infomaximum.com>
2025-07-28 13:22:28 +0300

    chore: выпуск версии

--- scripts/re-release.mjs
+++ scripts/re-release.mjs
@@ -1,19 +1,11 @@
 //@ts-check
 
 import { execSync } from "child_process";
-import { readFileSync } from "fs";
-import { resolve } from "path";
-import semver from "semver";
-
-const packageJsonPath = resolve(process.cwd(), "package.json");
-const packageData = JSON.parse(readFileSync(packageJsonPath, "utf-8"));
-const baseVersion = semver.valid(semver.coerce(packageData.version));
 
 const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
-const branchName = execSync("git rev-parse --abbrev-ref HEAD").toString().trim() || "unknown";
+const branchName =
+  execSync("git rev-parse --abbrev-ref HEAD").toString().trim() || commitHash || "unknown";
 
-const newVersion = `${baseVersion}-${commitHash}-${branchName}`;
-
-execSync(`npx standard-version --release-as ${newVersion}`, {
+execSync(`npx standard-version --prerelease ${branchName}`, {
   stdio: "inherit",
 });

55e65694b9aa03bebaf638bf9156f719aa884cd1
Dmitry Balaev <d.balaev@Infomaximum.biz>
2025-10-22 09:34:17 +0300

    feat: IDefinition расширен методом createSettingsScheme

--- src/definition.ts
+++ src/definition.ts
@@ -5,16 +5,32 @@ import type { IBaseWidgetSettings } from "./settings/baseWidget";
 import type { StringKeyOf } from "./utilityTypes";
 import type { IFillSettings } from "./widgetApi";
 import type { IGlobalContext } from "./widgetContext";
+import type { z as Zod, ZodSchema } from "zod";
+
+/** Используется для вывода типа настроек виджета по описанной схеме в методе `createSettingsScheme`
+ */
+export type TSettingsOf<
+  D extends IDefinition<B, any, any>,
+  B extends IBaseWidgetSettings = IBaseWidgetSettings,
+> = D["createSettingsScheme"] extends (z: typeof Zod) => infer Schema
+  ? Schema extends ZodSchema
+    ? Zod.infer<Schema> extends infer T
+      ? T & B
+      : B
+    : B
+  : B;
 
 export interface IDefinition<
-  WidgetSettings extends IBaseWidgetSettings,
-  GroupSettings extends IGroupSettings,
+  WidgetSettings extends IBaseWidgetSettings = IBaseWidgetSettings,
+  GroupSettings extends IGroupSettings = IGroupSettings,
   MigrationStruct extends TMigrationStruct = IWidgetStruct,
 > {
   /** иконка виджета отображаемая в системе (в base64, svg или png) */
   icon?: string;
   /** метод удаляет настройки, наследуемые от темы и возвращает функцию отката, которая возвращает удаленные настройки */
   cleanupThemeProperties?: (settings: WidgetSettings) => (settings: WidgetSettings) => void;
+  /** возвращает zod-схему настроек виджета */
+  createSettingsScheme?: (z: typeof Zod) => ZodSchema<WidgetSettings>;
   /** возвращает конфигурацию настроек для отображения */
   createPanelDescription: IPanelDescriptionCreator<WidgetSettings, GroupSettings>;
   /** заполняет настройки значениями по умолчанию */

d3e89bf50ee35a3ccce10b1f9fe36194e925f4e5
Dmitry Balaev <d.balaev@Infomaximum.biz>
2026-02-27 18:10:13 +0300

    feat: отказ от настроек текста виджетов в WidgetPresetSettings

--- src/preset.schema.ts
+++ src/preset.schema.ts
@@ -1,15 +1,8 @@
 import { BaseWidgetSettingsSchema, type TZod } from ".";
 
 export const WidgetPresetSettingsSchema = (z: TZod) =>
-  BaseWidgetSettingsSchema(z)
-    .pick({
-      filterMode: true,
-      ignoreFilters: true,
-      stateName: true,
-      titleColor: true,
-      titleSize: true,
-      titleWeight: true,
-    })
-    .extend({
-      textSize: z.number().default(12),
-    });
+  BaseWidgetSettingsSchema(z).pick({
+    filterMode: true,
+    ignoreFilters: true,
+    stateName: true,
+  });
